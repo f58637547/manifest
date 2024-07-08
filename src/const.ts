c23ebe018713615b2c2c65084a235b1f66503d90
f58637547 <fomo@LAPTOP-0M49R8JS>
2024-07-07 22:01:27 -0500

    :sparkles: feat: 2.0

--- src/const.ts
+++ src/const.ts
@@ -2,4 +2,4 @@ import { resolve } from 'node:path';
 
 export const root = resolve(__dirname, '..');
 export const pluginDir = resolve(root, './public');
-export const BASE_URL = 'https://f58637547.github.io/manifest/public';
+export const BASE_URL = 'https://openai-collections.chat-plugin.lobehub.com';
